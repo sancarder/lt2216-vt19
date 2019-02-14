b58a12b4e7f507f380fc3d2927e52e80cdf45f8c
Vladislav Maraev <vlad@maraev.me>
2019-02-14 15:10:56 +0100

    Add flask route for lab2

--- vxml-server/app.py
+++ vxml-server/app.py
@@ -17,6 +17,13 @@ def lab1():
     response.headers["Content-Type"] = "application/xml"
     return response
 
+@app.route('/lab2')
+def lab2():
+    vxml = render_template('lab2.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
 @app.route('/grammars/<path:path>')
 def send_grammar(path):
     return send_from_directory('grammars', path)
